cf27756a40c49fc884dca7612bb8e10921fd065d
Dougal Matthews <dougal85@gmail.com>
2012-04-10 14:47:44 +0100

    Squashed commit of the following:
    commit 5d4b1b40d932fc3866663d7a486be63ac3f8f2ab
    Author: Dougal Matthews <dougal85@gmail.com>
    Date:   Mon Apr 9 15:51:00 2012 +0100
    
        sessionStorage and custom storage.
    
        Session storage can now we used with all the
        standard API calls. Using set for example;
    
        ``locache.session.set("…")```
    
        Custom locache objects can now we created too.
        With locache.createCache(), by default this will
        use localStorage, but you can pass in another
        backend like sessionStorage;
    
        ``locache.createcache({storage:locache.backends.session})``
    
        Or, your own custom storage object.
    
    commit 6129cd255c5f847292e136e61615b00344c4f5d6
    Author: Dougal Matthews <dougal85@gmail.com>
    Date:   Mon Apr 9 15:06:34 2012 +0100
    
        Initial refactor steps.
    
        Add a backend for sessionStorage, remove all
        assumptions of localStorage by adding an enabled
        function to the storage backends.

--- locache.js
+++ locache.js
@@ -6,7 +6,7 @@
 //      locache may be freely distributed under the MIT licence.
 //
 //      locache is a client side caching framework that stores data
-//      is localStorage and proves a memcache inspired API for
+//      with DOM Storage and proves a memcache inspired API for
 //      setting and retrieving values.
 
 //
@@ -21,28 +21,39 @@
     // Save a reference to the global window object.
     var root = this
 
+    function LocacheCache(options){
+
+        for (var key in options) {
+            if (options.hasOwnProperty(key)) {
+                this[key] = options[key]
+            }
+        }
+
+    }
+
     // The top-level namespace. All public locache objects will be
     // attached to this object.
-    var locache = {}
+    var locache = new LocacheCache()
+
     // Attach the locache namespace to the global window object.
     root.locache = locache
 
     // Current version of locache. Keep this in sync with the version
     // at the top of this file.
-    locache.VERSION = "VERSION-PLACEHOLDER"
+    LocacheCache.prototype.VERSION = "VERSION-PLACEHOLDER"
 
     // Boolean value that determines if they browser supports localStorage or
     // not. This is based on the Modernizr implementation that can be found
     // in [the Modernizr GitHub repository.](https://github.com/Modernizr/Modernizr/blob/c56fb8b09515f629806ca44742932902ac145302/modernizr.js#L696-731)
-    locache.supportsLocalStorage = (function() {
+    LocacheCache.prototype.supportsLocalStorage = (function() {
 
         try {
             // Create a test value and attempt to set, get and remove the
             // value. These are the core functionality required by locache.
             var test_val = "___locache___"
-            localStorage.setItem(test_val, test_val)
-            localStorage.getItem(test_val)
-            localStorage.removeItem(test_val)
+            window.localStorage.setItem(test_val, test_val)
+            window.localStorage.getItem(test_val)
+            window.localStorage.removeItem(test_val)
             // If any of the checks fail, an exception will be raised. At
             // that point we can flag the browser as not supporting
             // localStorage.
@@ -53,47 +64,106 @@
 
     })()
 
+    // Boolean value that determines if they browser supports sessionStorage or
+    // not. This is based on the Modernizr implementation that can be found
+    // in [the Modernizr GitHub repository.](https://github.com/Modernizr/Modernizr/blob/c56fb8b09515f629806ca44742932902ac145302/modernizr.js#L696-731)
+    LocacheCache.prototype.supportsSessionStorage = (function() {
+
+        try {
+            // Create a test value and attempt to set, get and remove the
+            // value. These are the core functionality required by locache.
+            var test_val = "___locache___"
+            window.sessionStorage.setItem(test_val, test_val)
+            window.sessionStorage.getItem(test_val)
+            window.sessionStorage.removeItem(test_val)
+            // If any of the checks fail, an exception will be raised. At
+            // that point we can flag the browser as not supporting
+            // sessionStorage.
+            return true
+        } catch(e) {
+            return false
+        }
+
+    })()
+
     // Boolean flag to check if the browser supports native JSON.
-    locache.supportsNativeJSON = !!window.JSON
+    LocacheCache.prototype.supportsNativeJSON = !!window.JSON
 
     // Internal utility functions
     // --------------------
 
     // Two cache prefixes. When storing values, all keys are prefixed
-    // to avoid collisions with other usage of localStorage. If the
-    // stored value is given an expire time then a second key is set
-    // with a different prefix to store this time.
-    locache.cachePrefix = '___locache___'
-    locache.expirePrefix = '___locacheExpire___'
+    // to avoid collisions with other usage of the storage backend.
+    // If the stored value is given an expire time then a second key
+    // is set with a different prefix to store this time.
+    LocacheCache.prototype.cachePrefix = '___locache___'
+    LocacheCache.prototype.expirePrefix = '___locacheExpire___'
 
-    // A simple wrapper around localStorage for usage interlally within
-    // locache. This is added to offer a level of abstraction so the
-    // storage system can be changed to support any browser oddities.
-    locache.storage = {
+    // Built in locache backends. These are simple wrappers around the actual
+    // storage mechanism to allow for them to be easily exchanged.
 
-        set : function(key, value){
-            return localStorage.setItem(key, value)
-        },
+    LocacheCache.prototype.backends = {
+        // Wrapper around localStorage - persistent local storage in the
+        // browser.
+        local: {
+            set : function(key, value){
+                return window.localStorage.setItem(key, value)
+            },
 
-        get : function(key, value){
-            return localStorage.getItem(key)
-        },
+            get : function(key, value){
+                return window.localStorage.getItem(key)
+            },
 
-        remove : function(key){
-            return localStorage.removeItem(key)
-        },
+            remove : function(key){
+                return window.localStorage.removeItem(key)
+            },
 
-        length : function(key){
-            return localStorage.length
-        },
+            length : function(key){
+                return window.localStorage.length
+            },
 
-        key : function(index){
-            if (index < 0 || index >= this.length()){
-                return
+            key : function(index){
+                if (index < 0 || index >= this.length()){
+                    return
+                }
+                return window.localStorage.key(index)
+            },
+            enabled: function(){
+                return locache.supportsLocalStorage
             }
-            return localStorage.key(index)
+        },
+        // Wrapper around sessionStorage - storage in the browser that is
+        // cleared each time a new session is started - new browser window etc.
+        session : {
+            set : function(key, value){
+                return window.sessionStorage.setItem(key, value)
+            },
+
+            get : function(key, value){
+                return window.sessionStorage.getItem(key)
+            },
+
+            remove : function(key){
+                return window.sessionStorage.removeItem(key)
+            },
+
+            length : function(key){
+                return window.sessionStorage.length
+            },
+
+            key : function(index){
+                if (index < 0 || index >= this.length()){
+                    return
+                }
+                return window.sessionStorage.key(index)
+            },
+            enabled: function(){
+                return locache.supportsSessionStorage
+            }
         }
     }
+
+    LocacheCache.prototype.storage = locache.backends.local;
 
     // Utility method to get the number of milliseconds since the Epoch. This
     // is used when comparing keys to see if they have expired.
@@ -102,19 +172,19 @@
     }
 
     // Given a key, return the key used internally for storing values without
-    // the risk of collisions over usage of localStorage.
-    locache.key = function(key){
+    // the risk of collisions over usage of the storage backend.
+    LocacheCache.prototype.key = function(key){
         return this.cachePrefix + key
     }
 
     // Given a key, return the key to be used internally for expiry time.
-    locache.expirekey = function(key){
+    LocacheCache.prototype.expirekey = function(key){
         return this.expirePrefix + key
     }
 
     // Given a key, look up its expire time and determine if its in the past
     // or not. Returns a Boolean.
-    locache.hasExpired = function(key){
+    LocacheCache.prototype.hasExpired = function(key){
 
         var expireKey = this.expirekey(key)
         var expireValue = parseInt(this.storage.get(expireKey), 10)
@@ -132,12 +202,12 @@
     // --------------------
 
     // Given a key, a value and an optional number of seconds store the value
-    // in localStorage.
-    locache.set = function(key, value, seconds){
+    // in the storage backend.
+    LocacheCache.prototype.set = function(key, value, seconds){
 
-        // If localStorage isn't supported or the key passed in is falsy,
-        // perform a no-op.
-        if (!this.supportsLocalStorage || !key) return
+        // If the storage backend isn't supported or the key passed in is
+        // falsy, perform a no-op.
+        if (!this.storage.enabled() || !key) return
 
         var expireKey = this.expirekey(key)
         var valueKey = this.key(key)
@@ -159,13 +229,13 @@
 
     // Fetch a value from the cache. Either returns the value, or if it
     // doesn't exist (or has expired) return null.
-    locache.get = function(key){
+    LocacheCache.prototype.get = function(key){
 
-        // If localStorage isn't supported perform a no-op.
-        if (!this.supportsLocalStorage) return null
+        // If the storage backend isn't enabled perform a no-op.
+        if (!this.storage.enabled()) return null
 
         // If the value has expired, before returning null remove the key
-        // from localStorage to free up the space.
+        // from the storage backend to free up the space.
         if (this.hasExpired(key)){
             this.remove(this.key(key))
             return null
@@ -195,10 +265,10 @@
 
     // When removing a key - delete from the storage both the value key/value
     // pair and the expiration time key/value pair.
-    locache.remove = function(key){
+    LocacheCache.prototype.remove = function(key){
 
-        // If localStorage isn't supported perform a no-op.
-        if (!this.supportsLocalStorage) return
+        // If the storage backend isn't enabled perform a no-op.
+        if (!this.storage.enabled()) return
 
         var expireKey = this.expirekey(key)
         var valueKey = this.key(key)
@@ -213,10 +283,10 @@
     // the increment. The fetched value is always parsed as an int to make
     // sure the increment will work - this means if a non-int was stored, it
     // will be converted first and thus reset the counter to zero.
-    locache.incr = function(key){
+    LocacheCache.prototype.incr = function(key){
 
-        // If localStorage isn't supported perform a no-op.
-        if (!this.supportsLocalStorage) return
+        // If the storage backend isn't enabled perform a no-op.
+        if (!this.storage.enabled()) return
 
         var current = parseInt(this.get(key), 10)
         if (!current){
@@ -229,10 +299,10 @@
     }
 
     // Exactly the same as the incr function, but with a decrementing value.
-    locache.decr = function(key){
+    LocacheCache.prototype.decr = function(key){
 
-        // If localStorage isn't supported perform a no-op.
-        if (!this.supportsLocalStorage) return
+        // If the storage backend isn't enabled perform a no-op.
+        if (!this.storage.enabled()) return
 
         var current = parseInt(this.get(key), 10)
         if (!current){
@@ -246,17 +316,17 @@
 
     // Given a properties object, in the form of {key: value, key:value} set
     // multiple keys.
-    locache.setMany = function(properties, seconds){
+    LocacheCache.prototype.setMany = function(properties, seconds){
 
-        // If localStorage isn't supported perform a no-op.
-        if (!this.supportsLocalStorage) return
+        // If the storage backend isn't enabled perform a no-op.
+        if (!this.storage.enabled()) return
 
         // Iterate through all the object properties.
         for (var key in properties) {
             // Ignore any inherited properties, by making sure they are in
             // the given objecct.
             if (properties.hasOwnProperty(key)) {
-                locache.set(key, properties[key], seconds)
+                this.set(key, properties[key], seconds)
             }
         }
 
@@ -264,15 +334,15 @@
 
     // Given an array of keys, return an array of values. If values don't
     // exist, null will be in their place.
-    locache.getMany = function(keys){
+    LocacheCache.prototype.getMany = function(keys){
 
         var results = []
 
         for (var i=0; i < keys.length; i++){
             // To ensure that the correct structure is returned, if
-            // localStorage isn't supported return an array of null values
-            // with the correct length.
-            if (this.supportsLocalStorage){
+            // the storage backend isn't enabled return an array of null
+            // values with the correct length.
+            if (this.storage.enabled()){
                 results.push(this.get(keys[i]))
             } else {
                 results.push(null)
@@ -284,10 +354,10 @@
     }
 
     // Given an array of keys, remove all of them from the cache.
-    locache.removeMany = function(keys){
+    LocacheCache.prototype.removeMany = function(keys){
 
-        // If localStorage isn't supported perform a no-op.
-        if (!this.supportsLocalStorage) return
+        // If the storage backend isn't enabled perform a no-op.
+        if (!this.storage.enabled()) return
 
         for (var i=0; i < keys.length; i++){
             this.remove(keys[i])
@@ -296,17 +366,17 @@
     }
 
     // Delete all stored values from the cache. This method will only remove
-    // values added to localStorage with the locache prefix in the key.
-    locache.flush = function(){
+    // values added to the storage backend with the locache prefix in the key.
+    LocacheCache.prototype.flush = function(){
 
-        // If localStorage isn't supported perform a no-op.
-        if (!this.supportsLocalStorage) return
+        // If the storage backend isn't enabled perform a no-op.
+        if (!this.storage.enabled()) return
 
         var length = this.storage.length()
         var prefix = this.cachePrefix
 
-        // Iteratate through all the keys stored in localStorage - if the key
-        // starts with the prefix cache prefix, then remove that key.
+        // Iteratate through all the keys stored in the storage backend - if
+        // the key tarts with the prefix cache prefix, then remove that key.
         for (var i=0; i < length; i++) {
             var key = this.storage.key(i)
             if (key && key.indexOf(prefix) === 0) this.storage.remove(key)
@@ -314,12 +384,13 @@
 
     }
 
-    // Return the number of cache values stored in localStorage. This only
-    // calculates the values stored by locache.
-    locache.length = function(){
+    // Return the number of cache values stored in the storage backend. This
+    // only calculates the values stored by locache.
+    LocacheCache.prototype.length = function(){
 
-        // If localStorage isn't supported perform a no-op and return zero.
-        if (!this.supportsLocalStorage) return 0
+        // If the storage backend isn't supported perform a no-op and return
+        // zero.
+        if (!this.storage.enabled()) return 0
 
         var c = 0
         var length = this.storage.length()
@@ -334,13 +405,13 @@
     }
 
     // A cleanup utility method to remove expired keys. Iterate through all
-    // the keys stored inlocalStorage. If they key is a locache key (it has
-    // the prefix) then check to see if the key has expired. If it has,
-    // remove the key from the cache.
-    locache.cleanup = function(){
+    // the keys stored in the storage backend. If they key is a locache key
+    // (it has the prefix) then check to see if the key has expired. If it
+    // has, remove the key from the cache.
+    LocacheCache.prototype.cleanup = function(){
 
-        // If localStorage isn't supported perform a no-op.
-        if (!this.supportsLocalStorage) return
+        // If the storage backend isn't enabled perform a no-op.
+        if (!this.storage.enabled()) return
 
         var length = this.storage.length()
         var prefix = this.cachePrefix
@@ -360,5 +431,14 @@
         }
 
     }
+
+    LocacheCache.prototype.createCache = function(options){
+        return new LocacheCache(options)
+    }
+
+    locache.session = new LocacheCache({
+        storage: locache.backends.session
+    })
+
 
 }).call(this);
