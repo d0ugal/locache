30135f558944794d4f8aa26ba107ff0ccb88381e
Dougal Matthews <dougal85@gmail.com>
2012-04-11 12:37:24 +0100

    Re-factored the test suite.
    Now runs a full set of tests for no dom storage, 
    local, session and more tests for custom storage
    objects and the expiration calculations.

---
+++ tests/specs/custom-storage.js
@@ -0,0 +1,72 @@
+/*jshint asi:true */
+
+describe("Custom Storage:", function(){
+
+    "use strict";
+
+    beforeEach(function(){
+        // Define the storage API with no-op functions for stubbing below.
+        // Only enabled returns True, as otherwise its backend is never
+        // touched (for disabling when browsers don't support a feature, like
+        // localStorage).
+        var f = function(){}
+        this.storage = { set: f, get: f, remove: f, length: f, key: f,
+            enabled: function(){ return true}}
+
+        // Create a cache object to re-use, let it create with the default storage
+        // and then we will augment it with the mock in each test.
+        this.cache = locache.createCache({storage: this.storage})
+
+    })
+
+    it("should call 'set' on the storage object once when an expire isn't provided", function(){
+        var spy = sinon.spy(this.storage, 'set')
+        this.cache.set("foo", "bar")
+        expect(spy.calledOnce).toBe(true)
+    })
+
+    it("should call 'set' on the storage object twice when an expire is provided", function(){
+        // When setting with an expire value, we need to set the expire key
+        // along with the value key. Thus, two calls to storage.set will be
+        // made.
+        var spy = sinon.spy(this.storage, 'set')
+        this.cache.set("foo", "bar", 60)
+        expect(spy.calledTwice).toBe(true)
+    })
+
+    it("shouldn't call 'set' when the key is falsy", function(){
+        var spy = sinon.spy(this.storage, 'set')
+        this.cache.get('', 'value', 50)
+        expect(spy.callCount).toBe(0)
+    })
+
+
+    it("should call 'get' twice when fetching a key", function(){
+        // When getting an object, a lookup to the expire key is also made.
+        var spy = sinon.spy(this.storage, 'get')
+        this.cache.get("foo")
+        expect(spy.calledTwice).toBe(true)
+    })
+
+    it("shouldn't call 'get' when the key is falsy", function(){
+        // When getting an object, a falsy key, or no key shouldn't do
+        // anything
+        var spy = sinon.spy(this.storage, 'get')
+        this.cache.get()
+        expect(spy.callCount).toBe(0)
+    })
+
+    it("shouldn't call any functions if the storage is disabled", function(){
+
+        // Stub the storage object to make enabled return false.
+        sinon.stub(this.storage, 'enabled').returns(false);
+
+        var spy = sinon.spy(this.storage, 'set')
+        this.cache.get('foo', 'bar', 50)
+        // Since enabled is false, the set call shouldn't be passed to the
+        // storage backend, and thus set wont be called.
+        expect(spy.callCount).toBe(0)
+
+    })
+
+})
---
+++ tests/specs/expire.js
@@ -0,0 +1,113 @@
+/*jshint asi:true */
+
+describe("Expire Calculations:", function(){
+
+    "use strict";
+
+    beforeEach(function(){
+
+        // Create a partial storage (implementing set, get and remove as
+        // needed for these tests) that simply stores in an object.
+
+        var store = {}
+        this.store = store
+        this.storage = {
+            set : function(key, value){ return store[key] = value },
+            get : function(key, value){ return store[key] || null },
+            remove : function(key){ delete store[key] },
+            enabled: function(){ return true },
+            length: function(){
+                var keys = []
+                for (var key in store) {
+                    if (store.hasOwnProperty(key)) {
+                        keys.push(key)
+                    }
+                }
+                return keys.length
+            },
+            key: function(i){
+                var keys = []
+                for (var key in store) {
+                    if (store.hasOwnProperty(key)) {
+                        keys.push(key)
+                    }
+                }
+                return keys[i]
+            }
+        }
+
+        this.cache = locache.createCache({storage:this.storage})
+
+        this.now = new Date().getTime()
+        this.past = this.now / 10
+        this.future = this.now * 10
+
+    })
+
+    it("should test key has expired", function(){
+
+        var key =  "mykey", expireKey = this.cache.expirePrefix + key
+
+        // Now manually set the expire date as in the past.
+        this.store[expireKey] = this.past
+        expect(this.cache.hasExpired(key)).toBe(true)
+
+        // set the expire to "now", minus a second to make sure its just
+        // expired.
+        this.store[expireKey] =  this.now - 1
+        expect(this.cache.hasExpired(key)).toBe(true)
+
+    })
+
+    it("should test key has not expired", function(){
+
+        var key =  "mykey", expireKey = this.cache.expirePrefix + key
+
+        // set the expire to "now", plus 1 seconds to make sure its not quite
+        // expired yet.
+        this.store[expireKey] =  this.now + 1
+        expect(this.cache.hasExpired(key)).toBe(false)
+
+        // Finally, test the future.
+        this.store[expireKey] =  this.future
+        expect(this.cache.hasExpired(key)).toBe(false)
+
+    })
+
+
+    it("should test cleaning up expired values", function(){
+
+        var key1 =  "mykey1",
+            cacheKey1 = this.cache.cachePrefix + key1,
+            expireKey1 = this.cache.expirePrefix + key1,
+            key2 =  "mykey2",
+            cacheKey2 = this.cache.cachePrefix + key2,
+            expireKey2 = this.cache.expirePrefix + key2
+
+        // Bypass the normal setting mechanisims by manually calling the
+        // storage wrapper around localStorage.
+        this.store[cacheKey1] = "value1"
+        this.store[cacheKey2] = "value2"
+
+        // set the first value to expire on a date in the past, and then
+        // second to expire in the future.
+        this.store[expireKey1] = this.past
+        this.store[expireKey2] = this.future
+
+        // Both values should be stored in localStorage - by passing the
+        // normal get method to avoid the checks for validation
+        expect(this.store[cacheKey1]).toBe("value1")
+        expect(this.store[cacheKey2]).toBe("value2")
+
+        // Perform a cleanup.
+        this.cache.cleanup()
+
+        // Check the values again, the first should have been removed but the
+        // second should be as originally stored.
+        expect(this.store[cacheKey1]).toBe(undefined)
+        expect(this.cache.get(cacheKey1)).toBe(null)
+        expect(this.store[cacheKey2]).toBe("value2")
+
+    })
+
+})
